ae65f323b93891b0ec19917f63eaade2a7d11eb9
Therese Weidenstedt <kw222mi@student.lnu.se>
2025-11-11 16:23:26 +0100

    Add method for attack

--- Pokemon/Domain/PokemonCreature.cs
+++ Pokemon/Domain/PokemonCreature.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Pokemon.Domain
 {
@@ -17,8 +18,20 @@ namespace Pokemon.Domain
         }
         public void PrintInfo()
         {
-            Console.WriteLine($"{Name} {Level}");
+            Console.WriteLine($"{Name} (Level {Level})");
+
         }
+
+        public void UseSimpleAttack (string attackName, int basePower) {
+            if (string.IsNullOrWhiteSpace(attackName)) { throw new ArgumentException("Attacknamn saknas. Ange minst 1 tecken."); }
+
+            else if(basePower <= 0) { throw new ArgumentException("BasePower måste vara > 0."); }
+
+                int damage = basePower + Level;
+            Console.WriteLine($"{Name} använder {attackName} – Skada: {damage} ({basePower}+{Level})");
+
+        }
+
     }
 
    
--- Pokemon/Program.cs
+++ Pokemon/Program.cs
@@ -1,7 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Pokemon.Domain;
+﻿using Pokemon.Domain;
 
-Console.WriteLine("Applikationen startar");
 
-var pokemon = new PokemonCreature("Bulbasaur", 1);
-pokemon.PrintInfo();
+
+
+
+
+try
+{
+    var pokemon = new PokemonCreature("Bulbasaur", 1);
+    pokemon.PrintInfo();
+
+    pokemon.UseSimpleAttack("Tackle", 5);
+    //pokemon.UseSimpleAttack("Tackle", 0);
+    //pokemon.UseSimpleAttack("Tackle", -3);
+    //pokemon.UseSimpleAttack("", 5);
+    pokemon.UseSimpleAttack(" Leafage ", 11);
+}
+catch (ArgumentException ex)
+{
+    // Fångar valideringsfel (t.ex. attackName/basePower)
+    Console.WriteLine($"[FEL] {ex.Message}");
+}
+catch (Exception ex)
+{
+    // Fångar *oväntade* fel (t.ex. null-referenser eller skrivfel)
+    Console.WriteLine($"[OVÄNTAT FEL] {ex.Message}");
+}
+finally
+{
+    // (valfritt) kod som alltid körs, t.ex. “Programmet avslutas.”
+}
