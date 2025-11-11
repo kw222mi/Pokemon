806288b835990d146bf9c7a7e25d0ad839339ac5
Therese Weidenstedt <kw222mi@student.lnu.se>
2025-11-11 15:41:07 +0100

    Add project files.

---
+++ Pokemon/Domain/ElementType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon.Domain
+{
+    internal class ElementType
+    {
+        enum Element
+        {
+            Fire,
+            Water,
+            Grass
+        }
+    }
+}
