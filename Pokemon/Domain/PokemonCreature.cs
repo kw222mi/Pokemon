af1cffbdd399f956a33d7594da95387a55ae8d76
Therese Weidenstedt <kw222mi@student.lnu.se>
2025-11-11 15:53:54 +0100

    create pokemon and write info

---
+++ Pokemon/Domain/PokemonCreature.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon.Domain
+{
+    public class PokemonCreature
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public PokemonCreature(string name, int level) {
+            Name = name;
+            Level = level;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"{Name} {Level}");
+        }
+    }
+
+   
+}
--- Pokemon/Program.cs
+++ Pokemon/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Pokemon.Domain;
+
 Console.WriteLine("Applikationen startar");
+
+var pokemon = new PokemonCreature("Bulbasaur", 1);
+pokemon.PrintInfo();
